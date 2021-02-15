143dc0111d927aaabfdcdfe5af1568684959b08e
PranishShresth <yunche64@gmail.com>
2021-02-15 12:23:50 +1000

    added multer

---
+++ backend/utils/grid_fs.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const { Readable } = require("stream");
+
+module.exports = {
+  uploadFileHelper: async function (file) {
+    try {
+      //default connection
+      const db = mongoose.connection.db;
+      let bucket = new mongoose.mongo.GridFSBucket(db, {
+        bucketName: "files",
+      });
+
+      let uploadStream = await bucket.openUploadStream(file.fileName, {
+        metadata: {
+          user: "John",
+        },
+      });
+      function bufferToStream(binary) {
+        const readableStream = new Readable(binary);
+        readableStream.push(binary);
+        readableStream.push(null);
+        return readableStream;
+      }
+
+      const stream = bufferToStream(file.buffer);
+    } catch (err) {}
+  },
+};
