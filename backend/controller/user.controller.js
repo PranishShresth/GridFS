848a912c92de68023e333a97797daa82230e4f39
PranishShresth <yunche64@gmail.com>
2021-02-15 12:03:38 +1000

    Login added

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,6 +1,8 @@
 const User = require("../models/User.model");
 const { userSchema } = require("../config/validation");
-
+const bcrypt = require("bcrypt");
+const { jwt_token } = require("./../config/keys");
+const jwt = require("jsonwebtoken");
 module.exports = {
   register: async (req, res) => {
     try {
@@ -14,6 +16,28 @@ module.exports = {
       res.status(201).json({ msg: "User successfully created" });
     } catch (err) {
       return res.status(400).json({ msg: err.message });
+    }
+  },
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email }).exec();
+      if (user) {
+        const comparePassword = await bcrypt.compare(password, user.password);
+        if (comparePassword) {
+          const payload = {
+            username: user.username,
+            _id: user._id,
+            email: user.email,
+          };
+          const token = jwt.sign(payload, jwt_token, { expiresIn: "2h" });
+          return res.status(200).json({ user: payload, token });
+        }
+        return res.status(400).json({ msg: "Incorrect Password" });
+      }
+      return res.status(400).json({ msg: "Such email doesn't exist" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
     }
   },
 };
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,8 @@
-const { register } = require("./../controller/user.controller");
+const { register, login } = require("./../controller/user.controller");
 const express = require("express");
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
